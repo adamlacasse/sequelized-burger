72662f4cfe60ef850c119e229498f2e0663ec5bc
Adam LaCasse <adamlacasse@outlook.com>
2018-06-26 12:15:58 -0400

    rolling along

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,54 +1,44 @@
-// require in express, and set up routing for it, and bring in
-// the burger.js model file
-var express = require('express');
+var express = require("express");
+var db = require("../models");
 var router = express.Router();
-var burger = require('../models/burger.js');
-
-// add a '/' endpoint that redirects to the /index route
-router.get('/', function(req, res) {
-	res.redirect('/index');
+router.get("/", function (req, res) {
+    db.SeqBurger.findAll({
+        order:["burger_name"]
+    }).then(function (response) {
+        var data = {
+            allBurgers: response
+        }
+        res.render("index", data);
+    });
 });
 
-// add an '/index' endpoint that gets all the burgers
-// then renders the index file by passing in all the burgers
-// as an object for handlebars to use
-router.get('/index', function(req, res) {
-	burger.selectAll(function(data) {
-		var hbsObject = {burgers: data};
-		console.log(hbsObject);
-		res.render('index', hbsObject);
-	});
+router.post("/", function (req, res) {
+    db.SeqBurger.create({
+        burger_name: req.body.name
+    }).then(function (response) {
+        console.log(response);
+        res.redirect("/");
+    });
 });
 
-// add a '/burgers/insertOne' endpoint that posts the 
-// burger name the user entered then as a callback it
-// redirects back to the /index route
-router.post('/burgers/insertOne', function(req, res) {
-	burger.insertOne(['burger_name', 'devoured'], [req.body.name, false], function() {
-		res.redirect('/index');
-	});
+router.put("/update/:id", function (req, res) {
+    db.SeqBurger.update({
+        devoured: true,
+    },{
+            where: {
+                id: parseInt(req.params.id)
+            }
+        
+    });
+    res.redirect("/");
 });
 
-// add a '/burgers/updateOne/:id' route that updates
-// the status of the burger from being uneaten to eaten
-// then does a callback that redirects to the /index endpoint
-router.put('/burgers/updateOne/:id', function(req, res) {
-	var condition = 'id = ' + req.params.id;
-	console.log('condition', condition);
-
-	burger.updateOne({devoured: req.body.devoured}, condition, function() {
-		res.redirect('/index');
-	});
+router.delete("/delete/:id", function (req, res) {
+    db.SeqBurger.destroy({
+        where: {
+            id: parseInt(req.params.id)
+        }
+    });
+    res.redirect("/");
 });
-
-// router.delete('/burgers/deleteOne', function(req, res) {
-// 	var condition = 'id = ' + req.body.name;
-// 	console.log('condition', condition);
-
-// 	burger.deleteOne(condition, function() {
-// 		res.redirect('/index');
-// 	});
-// });
-
-// export the router (controller) back to the server
 module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,32 +1,25 @@
-// require in the orm file that will talk to the database
-var orm = require("../config/orm.js");
+module.exports = function (sequelize, DataTypes) {
+    var Burger = sequelize.define("Burger",
+        {
+            burger_name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    len: [1, 100]
+                }
+            },
+            devoured: {
+                type: DataTypes.BOOLEAN,
+                defaultValue: false,
+                allowNull: false
+            },
+            date: {
+                type: DataTypes.DATE,
+                defaultValue: sequelize.literal("CURRENT_TIMESTAMP")
+            }
+        }, {
+            timestamps: false
+        });
+    return Burger;
+}
 
-// create the burger variable that will be exported back to the controller
-var burger = {
-	// selectAll for getting all the burgers
-	selectAll: function(cb) {
-		orm.selectAll('burgers', function(res) {
-			cb(res);
-		});
-	},
-	// insertOne for adding a new burger
-	insertOne: function(cols, vals, cb) {
-		orm.insertOne('burgers', cols, vals, function(res) {
-			cb(res);
-		});
-	},
-	// updateOne for changing the burger status
-	updateOne: function(objColVals, condition, cb) {
-		orm.updateOne('burgers', objColVals, condition, function(res) {
-			cb(res);
-		});
-	},
-	deleteOne: function(condition, cb) {
-		orm.deleteOne('burgers', condition, function(res) {
-			cb(res);
-		});
-	}
-};
-
-// export burger back to the controller
-module.exports = burger;
